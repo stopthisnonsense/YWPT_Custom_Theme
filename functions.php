a39ca1a12ec12f2361427666a58e724ec6435637
Amarvin <anthony@glacial.com>
2021-11-01 14:34:06 -0400

    Create register user page in admin

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
- 
+
 function divichild_enqueue_scripts() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ));
@@ -10,8 +10,67 @@ add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
 
 //you can add custom functions below this line:
 
+if( function_exists('acf_add_options_page') ) {
+    function ywpt_register_users() {
+        acf_add_options_page(
+            [
+                'page_title' 	=> 'Register User',
+                'menu_title'	=> 'Register User',
+                'menu_slug' 	=> 'register-user',
+                'capability'	=> 'edit_posts',
+                'redirect'		=> false,
+                'position'	    => '4.1'
+            ]
+        );
+    }
+    add_action('acf/init', 'ywpt_register_users');
+    function ywpt_send_email() {
+        // $screen = get_current_screen();
+        // if( strpos($screen->ID, 'register-user' == true) ) {
+
+            $specificEmail = get_field('email', 'options');
+            $role = get_field('role', 'options');
+            $userData = get_user_by('email', $specificEmail);
+
+            if( $userData == false || empty($userData) ) {
+                wp_create_user( $specificEmail, wp_generate_password(), $specificEmail );
+                $wp_user_object = get_user_by('email', $specificEmail);
+                $wp_user_object->set_role($role);
+            }
+            update_field( 'email', '', 'options' );
+            update_field( 'role', 'coach', 'options' );
 
 
 
+            $userData = get_user_by('email', $specificEmail);
+            if( $userData == false || empty($userData) ) {
+                return;
+            }
+            $key = get_password_reset_key($userData);
+            $resetLink = $rp_link = '<a href="' .
+                network_site_url("wp-login.php?action=rp&key=$key&login=" .
+                rawurlencode($specificEmail), 'login') . '">' .
+                "Click Here To Set Your Password" .
+                '</a>';
+            // $userData = $userData->ID;
 
+            // $key = get_password_reset_key( $userData );
+            $message = '<html><body>';
+            $message .= '';
+            $message .= $resetLink;
+            $message .= '</html></body>';
 
+            $fromEmail = get_option('admin_email');
+            $subject = 'Welcome!';
+
+            $headers = array(
+                'Content-Type: text/html; charset=UTF-8',
+                'From: ASU <' . $fromEmail . '>'
+            );
+
+            wp_mail($specificEmail, $subject, $message, $headers);
+        }
+
+        // }
+        add_action('acf/save_post', 'ywpt_send_email', 15);
+    }
