0fa0244d42da1a6bd57b83313b90afd52e6a9b3a
StopNonsense <anthonyjmarvin@gmail.com>
2021-11-17 14:41:50 -0500

    Add some new stuff, like options page and redirect Disabled old user register functionality Nice and shiny login page.

--- functions.php
+++ functions.php
@@ -10,20 +10,70 @@ add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
 
 //you can add custom functions below this line:
 
+//Disable admin bar for non admins.
+
+
+function ywpt_admin_bar() {
+    if (!current_user_can('administrator') && !is_admin()) {
+        show_admin_bar(false);
+    }
+}
+
+add_action('after_setup_theme', 'ywpt_admin_bar');
+
+function ywpt_login_redirect( $redirect_to, $request, $user ) {
+    //is there a user to check?
+    if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+        //check for admins
+        if ( in_array( 'administrator', $user->roles ) ) {
+            // redirect them to the default place
+            return $redirect_to;
+        }
+        if( function_exists( 'ywpt_site_options' ) ) {
+            $dashboard_object = get_field( 'dashboard', 'options' );
+            if( isset( $dashboard_object ) ) {
+                return get_the_permalink( $dashboard_object );
+            }
+        }
+        return home_url();
+        // return get_the_permalink( 'dashboard' );
+    }
+    return $redirect_to;
+}
+
+add_filter( 'login_redirect', 'ywpt_login_redirect', 10, 3 );
+
+
+// add option to register user.
 if( function_exists('acf_add_options_page') ) {
-    function ywpt_register_users() {
+    // function ywpt_register_users() {
+    //     acf_add_options_page(
+    //         [
+    //             'page_title' 	=> 'Register User',
+    //             'menu_title'	=> 'Register User',
+    //             'menu_slug' 	=> 'register-user',
+    //             'capability'	=> 'edit_posts',
+    //             'redirect'		=> false,
+    //             'position'	    => '4.1'
+    //         ]
+    //     );
+    // }
+    // add_action('acf/init', 'ywpt_register_users');
+
+    function ywpt_site_options() {
         acf_add_options_page(
             [
-                'page_title' 	=> 'Register User',
-                'menu_title'	=> 'Register User',
-                'menu_slug' 	=> 'register-user',
+                'page_title' 	=> 'Site Options',
+                'menu_title'	=> 'Site Options',
+                'menu_slug' 	=> 'site-options',
                 'capability'	=> 'edit_posts',
                 'redirect'		=> false,
-                'position'	    => '4.1'
+                'position'	    => '4.2'
             ]
         );
     }
-    add_action('acf/init', 'ywpt_register_users');
+    add_action('acf/init', 'ywpt_site_options');
+
     function ywpt_send_email() {
         // $screen = get_current_screen();
         // if( strpos($screen->ID, 'register-user' == true) ) {
@@ -73,7 +123,7 @@ if( function_exists('acf_add_options_page') ) {
         }
 
         // }
-        add_action('acf/save_post', 'ywpt_send_email', 15);
+        // add_action('acf/save_post', 'ywpt_send_email', 15);
 
         add_action( 'show_user_profile', 'ywpt_user_profile_fields' );
         add_action( 'edit_user_profile', 'ywpt_user_profile_fields' );
@@ -108,3 +158,22 @@ if( function_exists('acf_add_options_page') ) {
         <?php # var_dump($user_id, $user_status);
          }
     }
+
+
+
+    function my_login_logo() { ?>
+        <style type="text/css">
+            body.login {
+                background:  url(<?= get_bloginfo( 'url' ); ?>/wp-content/uploads/2021/11/AdobeStock_340923854-scaled.jpeg);
+                background-size: cover;
+            }
+            #login h1 a {
+                background-image: url(<?= get_bloginfo( 'url' ); ?>/wp-content/uploads/2021/11/Reaching-Out-Logo_large_400dpi.jpg);
+                /* background-image: url(<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ) ); ?>); */
+                background-size: contain;
+                height: 100px;
+                width: 100px;
+            }
+        </style>
+    <?php }
+    add_action( 'login_enqueue_scripts', 'my_login_logo' );
